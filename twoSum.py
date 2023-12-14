8af08d3cf3dd14ee34919b91312ae685186c25fd
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2023-12-14 02:20:21 -0500

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,15 +1,9 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    let dic = new Object();
-    for (let i = 0; i < nums.length; i++){
-        compliment = target - nums[i]
-        if (dic.hasOwnProperty(compliment)){
-            return ([dic[compliment], i])
-        }
-        dic[nums[i]] = i;
-    }
-};
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range (len(nums)):
+            complement = target - nums[i]
+            if complement in dic:
+                return [dic[complement], i]
+            else:
+                dic[nums[i]] = i
