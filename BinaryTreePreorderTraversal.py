b35128669edfc4bec13ab69a9cbbf1acebec767e
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2024-01-04 09:44:18 -0500

    Create BinaryTreePreorderTraversal.py

---
+++ BinaryTreePreorderTraversal.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        #Preorder:    Root -> Left -> Right 
+
+        # Recursive solution
+        # if not root:
+        #     return []
+        # elif not root.left and not root.right: 
+        #     return [root.val]
+        # elif not root.left: #if left do not exist then just go right
+        #     return [root.val] + Solution.preorderTraversal(self, root.right)
+        # else: return [root.val] + Solution.preorderTraversal(self, root.left) +  Solution.preorderTraversal(self, root.right)
+
+
+        
+        #Iterative solution
+        res = []
+        stack = []
+        curr = root
+
+        while True:
+            if curr: #check if current node has a value
+                stack.append(curr) #store the current root node in a stack
+                res.append(curr.val) #append current root to res
+                curr = curr.left #traverse the left node
+
+            elif stack:  #if current node is empty, check stack to see if we traversed any nodes by checking the stack
+                curr = stack.pop() #if true then go back to previous node 
+                curr = curr.right #traverse right side after left (if right side is empty, the current node will traverse back again)
+            else:
+                break
+        return res
