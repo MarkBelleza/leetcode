67b1a87303e6bd20d27da86f2671232d37e43748
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2024-07-01 09:54:15 -0400

    Update MaximumSubarray.py

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
         current_max = nums[0]
+        max_sum = nums[0]
         
         for num in nums[1:]:
             current_max = max(current_max + num, num)
@@ -9,4 +9,5 @@ class Solution:
         
         return max_sum
     
-    #Time: O(n), Space: O(1)
+    #Time: O(n)
+    #Space: O(1)
