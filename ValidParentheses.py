bab5752f2c917b703ec2486ee737d9de11e1957d
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2024-07-02 11:10:31 -0400

    Update ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,18 +1,25 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) > 1:
-            stack = []
-            opening = {"(" : ")", "{" : "}", "[" : "]"}
-            for i in s:
-                if i in opening:
-                    stack.append(i)
-                elif stack:
-                    if (opening[stack.pop()] != i):
-                        return False
-                else:
-                    return False
-            if stack:
+        if len(s) <= 1:
+            return False
+
+        brackets = {")" : "(", "}" : "{", "]" : "["}
+        stack = []
+
+        #Make sure the opening brackets correspond with its closing brackets
+        for char in s:
+            if not char in brackets:
+                stack.append(char)
+            elif stack and brackets[char] == stack[-1]:
+                stack.pop()
+            else:
                 return False
+        
+        # #Make sure all opening brackets has a closing bracket
+        if len(stack) == 0:
             return True
         else:
             return False
+            
+        #Time: O(n)
+        #Space: O(n)
