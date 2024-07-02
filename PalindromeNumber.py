afdaf8e327d50dbebb7c09fa9e773ed2385070bf
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2024-07-01 22:47:27 -0400

    Update PalindromeNumber.py

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,9 +1,31 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        for i in range(len(x)):
-            if x[i] != x[len(x) - 1 - i]:
-                return False
-            if i >= (len(x) - 1 - i):
-                return True
+        # x = str(x)
+        # l = 0
+        # r = len(x) - 1
+
+        # while l < r:
+        #     if x[l] != x[r]:
+        #         return False
+        #     else:
+        #         l += 1
+        #         r -= 1
+        # return True
+        #Time: O(n)
+        #Space: O(1)
+
+
+        #Without converting to string
+        if x < 0:
+            return False
+
+        reverse = 0
+        original = x
+
+        while x > 0:
+            reverse = (reverse * 10) + (x % 10)
+            x //= 10
         
+        return reverse == original
+        #Time: O(n)
+        #Space: O(1)
