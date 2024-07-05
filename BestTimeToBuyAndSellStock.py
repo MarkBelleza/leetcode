89d2fab7419b24335a5104454f39c44a367f334e
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2024-07-05 01:03:43 -0400

    Update BestTimeToBuyAndSellStock.py

--- BestTimeToBuyAndSellStock.py
+++ BestTimeToBuyAndSellStock.py
@@ -1,12 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        left = 0
-        max_profit = 0
-        for right in range(left, len(prices)):
-            profit = prices[right] - prices[left]
-            if profit > 0 :
-                max_profit = max(profit, max_profit)
-            else:
-                left = right
-        return max_profit
-                
+        #Two pointer solution
+        # left = 0
+        # max_profit = 0
+        # for right in range(left, len(prices)):
+        #     profit = prices[right] - prices[left]
+        #     if profit > 0 :
+        #         max_profit = max(profit, max_profit)
+        #     else:
+        #         left = right
+        # return max_profit
+        
+
+        #Or
+        profit = 0
+        buy = prices[0]
+
+        for price in prices:
+            if price < buy: #if current price is lower than previous buy, then buy this one
+                buy = price
+            else: #if current price is higher than previous buy, then sell
+                profit = max(price - buy, profit)
+        return profit
+        #Time: O(n)
+        #Space: O(1)
