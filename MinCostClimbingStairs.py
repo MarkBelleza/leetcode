3c1f598ecf882e21fe2aa227eb3383e1f97db57c
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-01-27 11:58:22 -0500

    Update MinCostClimbingStairs.py

--- MinCostClimbingStairs.py
+++ MinCostClimbingStairs.py
@@ -1,13 +1,24 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        min_cost = [0, 0] #since step 1 and 2 is free they cost 0 each
-        for i in range(2, n+1):
-            next_cost = min(min_cost[i-2] + cost[i-2], 
-                            min_cost[i-1] + cost[i-1])
+        # n = len(cost)
+        # min_cost = [0, 0] #since step 1 and 2 is free they cost 0 each
+        # for i in range(2, n+1):
+        #     next_cost = min(min_cost[i-2] + cost[i-2], 
+        #                    min_cost[i-1] + cost[i-1])
 
-            min_cost.append(next_cost)
-        return min_cost[n]
+        #     min_cost.append(next_cost)
+        # return min_cost[n]
+
+        # or (same solution)
+
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i-2], dp[i-1]) + cost[i]
+        
+        return min(dp[-1], dp[-2])
         #DP, bottom up tabulation
         #time: O(n), space: O(n)
     
