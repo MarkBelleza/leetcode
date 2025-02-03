b2b3335010105bf0d7ba37f08a49bc8a64e78ee2
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-02-03 16:40:17 -0500

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,9 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = {}
-        for i in range (len(nums)):
-            complement = target - nums[i]
+        # for i in range (len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in dic:
+        #         return [dic[complement], i]
+        #     else:
+        #         dic[nums[i]] = i
+
+        for i, num in enumerate(nums):
+            complement = target - num
             if complement in dic:
                 return [dic[complement], i]
             else:
-                dic[nums[i]] = i
+                dic[num] = i
+        #Time: O(n)
+        #Space: O(n)
+
+        #Slower but less memory
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     for j in range(len(nums)):
+        #         if j != i and nums[j] == complement:
+        #             return [i, j]
+        #Time: O(n^2)
+        #Space: O(1)
