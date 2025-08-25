3305a25602bf11740dbb3e661d5fd988731fa52a
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-08-25 02:51:56 -0400

    Update MergeStringsAlternately.py

--- MergeStringsAlternately.py
+++ MergeStringsAlternately.py
@@ -1,33 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
-        
-        #ans = ""
-        #shortest = len(word1)
-
-        # if len(word1) > len(word2):
-        #     shortest = len(word2)
-
-        # while shortest > 0:
-        #     ans = ans + word1[0] + word2[0]
-        #     word1 = word1[1:]
-        #     word2 = word2[1:]
-        #     shortest -= 1
-
-        # return ans + word1 + word2
-        # Time: O(n)
-        # Space: O(n)
-
-        # or
-        
         ans = ""
-        while word1 or word2:
-            if word1:
-                ans += word1[0]
-                word1 = word1[1:]
-            if word2:
-                ans += word2[0]
-                word2 = word2[1:]
+        i = 0
+        j = 0
+
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                ans += word1[i]
+                i += 1
+            if j < len(word2):
+                ans += word2[j]
+                j += 1
         return ans
         # Time: O(n)
-        # Space: O(n)
+        # Space: O(n + m)
