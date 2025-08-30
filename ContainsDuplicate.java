7f78c227305e256abaf8a55d179c67e53752e26b
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-08-29 23:20:04 -0400

    Create ContainsDuplicate.java

---
+++ ContainsDuplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for(int i: nums){
+            if(s.contains(i)){
+                return true;
+            }
+            s.add(i);
+        }
+        return false;
+    }
+
+    //Time: O(n)
+    //Space: O(n)
+}
