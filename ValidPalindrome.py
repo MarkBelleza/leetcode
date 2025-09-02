7f1d15f39fefc31e7c7fb7afbcb64f72133ccafb
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-09-02 17:18:44 -0400

    Update ValidPalindrome.py

--- ValidPalindrome.py
+++ ValidPalindrome.py
@@ -4,18 +4,22 @@ class Solution:
         r = len(s) - 1
 
         while l < r:
-            if s[l].isalnum() and s[r].isalnum():
-                if s[l].lower() != s[r].lower():
-                    return False
-                else:
-                    l += 1
-                    r -= 1
             if not s[l].isalnum():
                 l += 1
+                continue #skip the rest of the code and start next iteration
+            
             if not s[r].isalnum():
                 r -= 1
+                continue
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
         
         return True
 
         # Time: O(n)
         # Space: O(1)
+            
