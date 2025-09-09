8f4e9c5ea583f745faf859ab577f414ecb4df5a4
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-09-09 19:25:46 -0400

    Update TwoSumII-InputArrayIsSorted.java

--- TwoSumII-InputArrayIsSorted.java
+++ TwoSumII-InputArrayIsSorted.java
@@ -2,21 +2,24 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int l = 0;
         int r = numbers.length - 1;
+        int[] ans = new int[2];
 
-        while (l < r){
-            int tar = numbers[l] + numbers[r];
-            if(tar == target){
-                int[] ans = {l+1, r+1};
+        while(l < r){
+            int curr = numbers[l] + numbers[r];
+            if (curr == target){
+                ans[0] = l + 1;
+                ans[1] = r + 1;
                 return ans;
-            }else if(tar < target){
+            }
+            else if(curr < target){
                 l++;
-            }else{
+            }
+            else{
                 r--;
             }
         }
-        int[] ans = {l+1, r+1};
         return ans;
     }
+    //Time: O(n)
+    //Space: O(1)
 }
-// Time: O(n)
-// Space: O(1)
