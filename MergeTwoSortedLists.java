291a9ae4644288a4b55404a70f24500e6aec53b6
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-09-11 23:42:37 -0400

    Update MergeTwoSortedLists.java

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -13,24 +13,23 @@ class Solution {
         ListNode dummy = new ListNode();
         ListNode ans = dummy;
 
-        while(list1 != null || list2 != null){
-            if (list1 != null && list2 != null ){
-                if (list1.val < list2.val){
-                    dummy.next = new ListNode(list1.val, null);
-                    list1 = list1.next;
-                }else{
-                    dummy.next = new ListNode(list2.val, null);
-                    list2 = list2.next;
-                }
-                dummy = dummy.next;
-            }
-            else if(list1 == null){
-                dummy.next = list2;
-                list2 = null;
+        while(list1 != null && list2 != null ){
+            if (list1.val < list2.val){
+                dummy.next = new ListNode(list1.val, null);
+                list1 = list1.next;
             }else{
-                dummy.next = list1;
-                list1 = null;
+                dummy.next = new ListNode(list2.val, null);
+                list2 = list2.next;
             }
+            dummy = dummy.next;
+        }
+        
+        if(list1 == null){
+            dummy.next = list2;
+            list2 = null;
+        }else{
+            dummy.next = list1;
+            list1 = null;
         }
         return ans.next;
     }
