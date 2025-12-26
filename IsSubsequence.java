1c3bd51d3764aec6dcb429ed26d26493aeb53449
MarkBelleza <98550319+MarkBelleza@users.noreply.github.com>
2025-12-25 23:12:08 -0500

    Update IsSubsequence.java

--- IsSubsequence.java
+++ IsSubsequence.java
@@ -25,3 +25,25 @@ class Solution {
 }
 // Time: O(t) the length of t, since we assume t may be longer than s in length
 // Space: O(1)
+
+// or
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int a = 0;
+
+        for (char i: t.toCharArray()){
+            if (a == s.length()){
+                return true;
+            }
+            if (i == s.charAt(a)){
+                a += 1;
+            }
+        }
+
+        if (a == s.length()){
+            return true;
+        }
+        return false;
+    }
+}
